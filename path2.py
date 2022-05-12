160ccab4510718fd4d87b4fed3224fb7bfba8885
tushar4303 <padhytushar@protonmail.com>
2022-05-13 04:16:27 +0530

    add path-ids for assets

--- path2.py
+++ path2.py
@@ -2,63 +2,192 @@ doc_ids = {
   "First year": {
         "IT":{
             "Semester I": {
-                "Hallticket": "pdfFiles/ExamTimetable/SE/SE_IT_Sem4_C-Scheme.pdf",
-                "calendar": "pdfFiles/ExamTimetable/SE/SE_IT_Sem4_C-Scheme.pdf",
-                "timetable": "pdfFiles/ExamTimetable/SE/SE_IT_Sem4_C-Scheme.pdf",
-                "seatNo": "pdfFiles/ExamTimetable/SE/SE_IT_Sem4_C-Scheme.pdf"
+                "Syllabus": "pdfFiles/Syllabus/FE/be_first-year-engineering_fe-all-branches-semester-12-rev-2019c-scheme.pdf",
+                "calendar": "pdfFiles/Academic_calender/academic_calendar.png",
+                "timetable": "pdfFiles/ExamTimetable/FE/SEM1_CBCGS_C Scheme-SH2021.pdf",
+                "seatNo": "pdfFiles/seat_no/SE IT/SE IT_May2022.pdf"
             },
             "Semester II": {
-                "Hallticket": "1Yt1fx5Wy_PeSDB5IDT8wlfAAYpsqCYgA",
-                "calendar": "1-FIyPjML3Dop4HyuIOY2pKDw4de7a5x5",
-                "timetable": "1fNkkPMdCwzwTiI_4oz5UEnLXeJu0Gks7",
+                "Syllabus": "pdfFiles/Syllabus/FE/be_first-year-engineering_fe-all-branches-semester-12-rev-2019c-scheme.pdf",
+                "calendar": "pdfFiles/Academic_calender/academic_calendar.png",
+                "timetable": "pdfFiles/ExamTimetable/FE/SEM2_CBCGS_C Scheme-SH2021.pdf",
+                "seatNo" : "pdfFiles/seat_no/SE IT/SE IT_May2022.pdf"
             }
         }
     },
   "Second year": {
         "IT":{
       "Semester III": {
-                "Hallticket": "1Yt1fx5Wy_PeSDB5IDT8wlfAAYpsqCYgA",
-                "calendar": "1-FIyPjML3Dop4HyuIOY2pKDw4de7a5x5",
-                "timetable": "1fNkkPMdCwzwTiI_4oz5UEnLXeJu0Gks7",
+                "Syllabus": "pdfFiles/Syllabus/SE/be_information-technology_second-year-se-semester-34-rev-2019c-scheme.pdf",
+                "calendar": "pdfFiles/Academic_calender/academic_calendar.png",
+                "timetable": "pdfFiles/ExamTimetable/SE/SE_IT_Sem4_C-Scheme.pdf",
+                "seatNo" : "pdfFiles/seat_no/SE IT/SE IT_May2022.pdf"
       },
       "Semester IV": {
-                "Hallticket": "1Yt1fx5Wy_PeSDB5IDT8wlfAAYpsqCYgA",
-                "calendar": "1-FIyPjML3Dop4HyuIOY2pKDw4de7a5x5",
-                "timetable": "1fNkkPMdCwzwTiI_4oz5UEnLXeJu0Gks7",
+                "Syllabus": "pdfFiles/Syllabus/SE/be_information-technology_second-year-se-semester-34-rev-2019c-scheme.pdf",
+                "calendar": "pdfFiles/Academic_calender/academic_calendar.png",
+                "timetable": "pdfFiles/ExamTimetable/SE/SE_IT_Sem4_C-Scheme.pdf",
+                "seatNo" : "pdfFiles/seat_no/SE IT/SE IT_May2022.pdf"
+            }
+       },
+        "Comps":{
+      "Semester III": {
+                "Syllabus": "pdfFiles/Syllabus/SE/be_computer-engineering_second-year-se-semester-34-rev-2019c-scheme.pdf",
+                "calendar": "pdfFiles/Academic_calender/academic_calendar.png",
+                "timetable": "pdfFiles/ExamTimetable/SE/SE_COMP_Sem4_C-Scheme.pdf",
+                "seatNo" : "pdfFiles/seat_no/SE IT/SE IT_May2022.pdf"
+      },
+      "Semester IV": {
+                "Syllabus": "pdfFiles/Syllabus/SE/be_computer-engineering_second-year-se-semester-34-rev-2019c-scheme.pdf",
+                "calendar": "pdfFiles/Academic_calender/academic_calendar.png",
+                "timetable": "pdfFiles/ExamTimetable/SE/SE_COMP_Sem4_C-Scheme.pdf",
+                "seatNo" : "pdfFiles/seat_no/SE IT/SE IT_May2022.pdf"
+            }
+       },
+        "Mech":{
+      "Semester III": {
+                "Syllabus": "pdfFiles/Syllabus/SE/be_mechanical-engineering_second-year-se-semester-34-rev-2019c-scheme.pdf",
+                "calendar": "pdfFiles/Academic_calender/academic_calendar.png",
+                "timetable": "pdfFiles/ExamTimetable/SE/SE_MECH_Sem4_C-Scheme.pdf",
+                "seatNo" : "pdfFiles/seat_no/SE IT/SE IT_May2022.pdf"
+      },
+      "Semester IV": {
+                "Syllabus": "pdfFiles/Syllabus/SE/be_mechanical-engineering_second-year-se-semester-34-rev-2019c-scheme.pdf",
+                "calendar": "pdfFiles/Academic_calender/academic_calendar.png",
+                "timetable": "pdfFiles/ExamTimetable/SE/SE_MECH_Sem4_C-Scheme.pdf",
+                "seatNo" : "pdfFiles/seat_no/SE IT/SE IT_May2022.pdf"
+            }
+       },
+       "Extc":{
+      "Semester III": {
+                "Syllabus": "pdfFiles/Syllabus/SE/be_electronics-and-telecommunication_second-year-se-semester-34-rev-2019c-scheme.pdf",
+                "calendar": "pdfFiles/Academic_calender/academic_calendar.png",
+                "timetable": "pdfFiles/ExamTimetable/SE/SE_EXTC_Sem4_C-Scheme.pdf",
+                "seatNo" : "pdfFiles/seat_no/SE IT/SE IT_May2022.pdf"
+      },
+      "Semester IV": {
+                "Syllabus": "pdfFiles/Syllabus/SE/be_electronics-and-telecommunication_second-year-se-semester-34-rev-2019c-scheme.pdf",
+                "calendar": "pdfFiles/Academic_calender/academic_calendar.png",
+                "timetable": "pdfFiles/ExamTimetable/SE/SE_EXTC_Sem4_C-Scheme.pdf",
+                "seatNo" : "pdfFiles/seat_no/SE IT/SE IT_May2022.pdf"
             }
        }
     },
   "Third year": {
-      "IT":{
+        "IT":{
       "Semester V": {
-                "Hallticket": "1Yt1fx5Wy_PeSDB5IDT8wlfAAYpsqCYgA",
-                "calendar": "1-FIyPjML3Dop4HyuIOY2pKDw4de7a5x5",
-                "timetable": "1fNkkPMdCwzwTiI_4oz5UEnLXeJu0Gks7",
-                "seatNo": ""
+                "Syllabus": "pdfFiles/Syllabus/TE/be_information-technology_third-year-te-semester-5-rev-2019-c-scheme.pdf",
+                "calendar": "pdfFiles/Academic_calender/academic_calendar.png",
+                "timetable": "pdfFiles/ExamTimetable/TE/TE_IT_Sem-6_C-Scheme.pdf",
+                "seatNo" : "pdfFiles/seat_no/SE IT/SE IT_May2022.pdf"
       },
       "Semester VI": {
-                "Hallticket": "1Yt1fx5Wy_PeSDB5IDT8wlfAAYpsqCYgA",
-                "calendar": "1-FIyPjML3Dop4HyuIOY2pKDw4de7a5x5",
-                "timetable": "1fNkkPMdCwzwTiI_4oz5UEnLXeJu0Gks7",
-                "seatNo": ""
-          }
-      }
-  },
+                "Syllabus": "pdfFiles/Syllabus/TE/be_information-technology_third-year-te-semester-6-rev-2019-c-scheme.pdf",
+                "calendar": "pdfFiles/Academic_calender/academic_calendar.png",
+                "timetable": "pdfFiles/ExamTimetable/TE/TE_IT_Sem-6_C-Scheme.pdf",
+                "seatNo" : "pdfFiles/seat_no/SE IT/SE IT_May2022.pdf"
+            }
+       },
+        "Comps":{
+      "Semester V": {
+                "Syllabus": "pdfFiles/Syllabus/TE/be_computer-engineering_third-year-terev-semester-56-cbsgs.pdf",
+                "calendar": "pdfFiles/Academic_calender/academic_calendar.png",
+                "timetable": "pdfFiles/ExamTimetable/TE/TE_COMP_Sem-6_C-Scheme.pdf",
+                "seatNo" : "pdfFiles/seat_no/SE IT/SE IT_May2022.pdf"
+      },
+      "Semester VI": {
+                "Syllabus": "pdfFiles/Syllabus/TE/be_computer-engineering_third-year-terev-semester-56-cbsgs.pdf",
+                "calendar": "pdfFiles/Academic_calender/academic_calendar.png",
+                "timetable": "pdfFiles/ExamTimetable/TE/TE_COMP_Sem-6_C-Scheme.pdf",
+                "seatNo" : "pdfFiles/seat_no/SE IT/SE IT_May2022.pdf"
+            }
+       },
+        "Mech":{
+      "Semester V": {
+                "Syllabus": "pdfFiles/Syllabus/TE/be_mechanical-engineering_third-year-te-semester-5-rev-2019-c-scheme.pdf",
+                "calendar": "pdfFiles/Academic_calender/academic_calendar.png",
+                "timetable": "pdfFiles/ExamTimetable/TE/TE_MECH_Sem-6_C-Scheme.pdf",
+                "seatNo" : "pdfFiles/seat_no/SE IT/SE IT_May2022.pdf"
+      },
+      "Semester VI": {
+                "Syllabus": "pdfFiles/Syllabus/TE/be_mechanical-engineering_third-year-te-semester-6-rev-2019-c-scheme.pdf",
+                "calendar": "pdfFiles/Academic_calender/academic_calendar.png",
+                "timetable": "pdfFiles/ExamTimetable/TE/TE_MECH_Sem-6_C-Scheme.pdf",
+                "seatNo" : "pdfFiles/seat_no/SE IT/SE IT_May2022.pdf"
+            }
+       },
+       "Extc":{
+      "Semester V": {
+                "Syllabus": "pdfFiles/Syllabus/TE/be_electronics-and-telecommunication_third-year-te-semester-5-rev-2019-c-scheme.pdf",
+                "calendar": "pdfFiles/Academic_calender/academic_calendar.png",
+                "timetable": "pdfFiles/ExamTimetable/TE/TE_EXTC_Sem-6_C-Scheme.pdf",
+                "seatNo" : "pdfFiles/seat_no/SE IT/SE IT_May2022.pdf"
+      },
+      "Semester VI": {
+                "Syllabus": "pdfFiles/Syllabus/TE/be_electronics-and-telecommunication_third-year-te-semester-6-rev-2019-c-scheme.pdf",
+                "calendar": "pdfFiles/Academic_calender/academic_calendar.png",
+                "timetable": "pdfFiles/ExamTimetable/TE/TE_EXTC_Sem-6_C-Scheme.pdf",
+                "seatNo" : "pdfFiles/seat_no/SE IT/SE IT_May2022.pdf"
+            }
+       }
+    },
   "Fourth year": {
-      "IT":{
+        "IT":{
       "Semester VII": {
-                "Hallticket": "1Yt1fx5Wy_PeSDB5IDT8wlfAAYpsqCYgA",
-                "calendar": "1-FIyPjML3Dop4HyuIOY2pKDw4de7a5x5",
-                "timetable": "1fNkkPMdCwzwTiI_4oz5UEnLXeJu0Gks7",
-                "seatNo": ""
+                "Syllabus": "pdfFiles/Syllabus/BE/be_information-technology_final-year-berev-semester-78-cbsgs.pdf",
+                "calendar": "pdfFiles/Academic_calender/academic_calendar.png",
+                "timetable": "pdfFiles/ExamTimetable/BE/BE_IT_Sem-VIII.pdf",
+                "seatNo" : "pdfFiles/seat_no/SE IT/SE IT_May2022.pdf"
       },
       "Semester VIII": {
-                "Hallticket": "1Yt1fx5Wy_PeSDB5IDT8wlfAAYpsqCYgA",
-                "calendar": "1-FIyPjML3Dop4HyuIOY2pKDw4de7a5x5",
-                "timetable": "1fNkkPMdCwzwTiI_4oz5UEnLXeJu0Gks7",
-                "seatNo": ""
-         }
-      }
-  }
+                "Syllabus": "pdfFiles/Syllabus/BE/be_information-technology_final-year-berev-semester-78-cbsgs.pdf",
+                "calendar": "pdfFiles/Academic_calender/academic_calendar.png",
+                "timetable": "pdfFiles/ExamTimetable/BE/BE_IT_Sem-VIII.pdf",
+                "seatNo" : "pdfFiles/seat_no/SE IT/SE IT_May2022.pdf"
+            }
+       },
+        "Comps":{
+      "Semester VII": {
+                "Syllabus": "pdfFiles/Syllabus/BE/be_computer-engineering_final-year-berev-semester-78-cbsgs.pdf",
+                "calendar": "pdfFiles/Academic_calender/academic_calendar.png",
+                "timetable": "pdfFiles/ExamTimetable/BE/BE_COMP_Sem VIII.pdf",
+                "seatNo" : "pdfFiles/seat_no/SE IT/SE IT_May2022.pdf"
+      },
+      "Semester VIII": {
+                "Syllabus": "pdfFiles/Syllabus/BE/be_computer-engineering_final-year-berev-semester-78-cbsgs.pdf",
+                "calendar": "pdfFiles/Academic_calender/academic_calendar.png",
+                "timetable": "pdfFiles/ExamTimetable/BE/BE_COMP_Sem VIII.pdf",
+                "seatNo" : "pdfFiles/seat_no/SE IT/SE IT_May2022.pdf"
+            }
+       },
+        "Mech":{
+      "Semester VII": {
+                "Syllabus": "pdfFiles/Syllabus/BE/be_mechanical-engineering_final-year-berev-semester-78-cbsgs.pdf",
+                "calendar": "pdfFiles/Academic_calender/academic_calendar.png",
+                "timetable": "pdfFiles/ExamTimetable/BE/BE_MECH_Sem-VIII.pdf",
+                "seatNo" : "pdfFiles/seat_no/SE IT/SE IT_May2022.pdf"
+      },
+      "Semester VIII": {
+                "Syllabus": "pdfFiles/Syllabus/BE/be_mechanical-engineering_final-year-berev-semester-78-cbsgs.pdf",
+                "calendar": "pdfFiles/Academic_calender/academic_calendar.png",
+                "timetable": "pdfFiles/ExamTimetable/BE/BE_MECH_Sem-VIII.pdf",
+                "seatNo" : "pdfFiles/seat_no/SE IT/SE IT_May2022.pdf"
+            }
+       },
+       "Extc":{
+      "Semester VII": {
+                "Syllabus": "pdfFiles/Syllabus/BE/be_electronics-and-telecommunication_final-year-berev-semester-78-cbsgs.pdf",
+                "calendar": "pdfFiles/Academic_calender/academic_calendar.png",
+                "timetable": "pdfFiles/ExamTimetable/BE/BE_EXTC Engg_Sem VIII.pdf",
+                "seatNo" : "pdfFiles/seat_no/SE IT/SE IT_May2022.pdf"
+      },
+      "Semester VIII": {
+                "Syllabus": "pdfFiles/Syllabus/BE/be_electronics-and-telecommunication_final-year-berev-semester-78-cbsgs.pdf",
+                "calendar": "pdfFiles/Academic_calender/academic_calendar.png",
+                "timetable": "pdfFiles/ExamTimetable/BE/BE_EXTC Engg_Sem VIII.pdf",
+                "seatNo" : "pdfFiles/seat_no/SE IT/SE IT_May2022.pdf"
+            }
+       }
+    },
 }
 
